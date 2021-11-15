3a33ecbaf0370602991c7b07b494b46a251ef84a
shahid987 <mohammedshahid724@gmail.com>
2021-11-15 15:44:15 -0600

    third commit

--- controllers/rocket.js
+++ controllers/rocket.js
@@ -34,15 +34,24 @@ exports.rocket_create_post = async function (req, res) {
     }
 };
 // Handle rocket delete form on DELETE.
-exports.rocket_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: rocket delete DELETE ' + req.params.id);
-};
+
+    exports.Rocket_delete = async function(req, res) { 
+        console.log("delete "  + req.params.id) 
+        try { 
+            result = await Rocket.findByIdAndDelete( req.params.id) 
+            console.log("Removed " + result) 
+            res.send(result) 
+        } catch (err) { 
+            res.status(500) 
+            res.send(`{"error": Error deleting ${err}}`); 
+        } 
+    }; 
 // Handle rocket update form on PUT.
 exports.rocket_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
     try { 
-        let toUpdate = await rocket.findById( req.params.id) 
+        let toUpdate = await Rocket.findById( req.params.id) 
         // Do updates of properties 
         if(req.body.rocket_type)  
                toUpdate.rocket_type = req.body.rocket_type; 
