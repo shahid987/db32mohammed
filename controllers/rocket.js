898933bf2a85fd71752c5c052a01c5dc89e48f79
shahid987 <mohammedshahid724@gmail.com>
2021-11-15 18:06:30 -0600

    committed

--- controllers/rocket.js
+++ controllers/rocket.js
@@ -4,16 +4,16 @@ exports.rocket_list = function (req, res) {
     res.send('NOT IMPLEMENTED: rocket list');
 };
 // for a specific rocket.
-    exports.rocket_detail = async function(req, res) { 
-        console.log("detail"  + req.params.id) 
-        try { 
-            result = await rocket.findById( req.params.id) 
-            res.send(result) 
-        } catch (error) { 
-            res.status(500) 
-            res.send(`{"error": document for id ${req.params.id} not found`); 
-        } 
-    }; 
+exports.rocket_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await rocket.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
 // Handle rocket create on POST.
 exports.rocket_create_post = async function (req, res) {
     console.log(req.body)
@@ -35,37 +35,37 @@ exports.rocket_create_post = async function (req, res) {
 };
 // Handle rocket delete form on DELETE.
 
-    exports.Rocket_delete = async function(req, res) { 
-        console.log("delete "  + req.params.id) 
-        try { 
-            result = await Rocket.findByIdAndDelete( req.params.id) 
-            console.log("Removed " + result) 
-            res.send(result) 
-        } catch (err) { 
-            res.status(500) 
-            res.send(`{"error": Error deleting ${err}}`); 
-        } 
-    }; 
+exports.rocket_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Rocket.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
 // Handle rocket update form on PUT.
-exports.rocket_update_put = async function(req, res) { 
+exports.rocket_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`) 
-    try { 
-        let toUpdate = await Rocket.findById( req.params.id) 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Rocket.findById(req.params.id)
         // Do updates of properties 
-        if(req.body.rocket_type)  
-               toUpdate.rocket_type = req.body.rocket_type; 
-        if(req.body.cost) toUpdate.cost = req.body.cost; 
-        if(req.body.size) toUpdate.size = req.body.size; 
-        let result = await toUpdate.save(); 
-        console.log("Sucess " + result) 
-        res.send(result) 
-    } catch (err) { 
-        res.status(500) 
+        if (req.body.rocket_type)
+            toUpdate.rocket_type = req.body.rocket_type;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`); 
-    } 
-}; 
+failed`);
+    }
+};
 
 // List of all rockets
 exports.rocket_list = async function (req, res) {
@@ -92,3 +92,55 @@ exports.rocket_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+exports.rocket_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Rocket.findById(req.query.id)
+        res.render('rocketdetail', {
+            title: 'Rocket Detail',
+            toShow: result
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a rocket. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.rocket_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('rocketcreate', {
+            title: 'rocket Create'
+        });
+    } catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a rocket. 
+// query provides the id 
+exports.rocket_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Rocket.findById(req.query.id) 
+        res.render('rocketupdate', { title: 'rocket Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+// Handle a delete one view with id from query 
+exports.rocket_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Rocket.findById(req.query.id) 
+        res.render('rocketdelete', { title: 'rocket Delete', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};  
--- routes/rocket.js
+++ routes/rocket.js
@@ -1,8 +1,14 @@
 var express = require('express');
 const rocket_controllers= require('../controllers/rocket');
-const rocket = require('../models/rocket');
+
 var router = express.Router();
 
 
 router.get('/',rocket_controllers.rocket_view_all_Page);
+
+router.get('/detail', rocket_controllers.rocket_view_one_Page);
+router.get('/create', rocket_controllers.rocket_create_Page);
+router.get('/update', rocket_controllers.rocket_update_Page);
+router.get('/delete', rocket_controllers.rocket_delete_Page);
+ 
 module.exports=router;
